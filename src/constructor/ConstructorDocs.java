da9abdf78544d8d6faf322ad82a40d93893f3462
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-03-30 21:22:30 +0545

    updated

--- src/constructor/ConstructorDocs.java
+++ src/constructor/ConstructorDocs.java
@@ -1,5 +1,15 @@
 package constructor;
 
 public class ConstructorDocs {
-
+/*
+ *  -------------------- Constructor ------------------------
+ *  # used to create object in java.
+ *  # special type of method which has same name as class name.
+ *  # no return type.
+ *  # if there is no any user defined constructor in class then java provide default constructor
+ *  	to create object.
+ *  # Types:
+ *  	1. Default constructor
+ *  	2. Parameterized constructor
+ */
 }
