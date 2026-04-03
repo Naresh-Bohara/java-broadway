9872ce222a8361347df0cf13719c6006ac3e0002
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-03 07:39:42 +0545

    updated

--- src/constructor/User.java
+++ src/constructor/User.java
@@ -1,19 +1,49 @@
 package constructor;
 
 public class User {
+	// instance variables
 	String username;
 	String password;
 	
 	// default constructor
-//	User(){
-//		username="root";
-//		password = "password";
-//	}
+	User(){
+		username="root";
+		password = "password";
+	}
 	
 	// parameterized constructor
-	User(String un, String psw){
-		username = un;
-		password = psw;
+	User(String username, String password){
+		
+		// constructor
+		//default
+//		this(); // default constructor
+//		this(123,323); //parameterized constructor
+		
+		// instance variables
+		this.username = username;
+		this.password = password;
+		
+		
+		// instance methods
+//		this.printUser();
+//		or
+//		printUser();
+		
+		/*
+		 * # 'this' keyword represents current object.
+		 * 
+		 * # we can call:
+		 *    -> instance variables,
+		 *    -> instance methods, 
+		 *    -> constructors
+		 */
+		
+	}
+	
+	// parameterized constructor
+	User(String username){
+
+		this.username = username;
 		
 	}
 	
@@ -25,6 +55,7 @@ public class User {
 	public static void main(String[] args) {
 		User u = new User("naresh", "password");
 		u.printUser();
+//		Object
 		
 		// above is equivalent to below:
 //		new User().printUser();
