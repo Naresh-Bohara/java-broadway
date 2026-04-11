3831db8b3e07f566a2c23ee921c8424a2bcbde72
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-11 08:23:33 +0545

    updated

--- src/arrayofobject/Test.java
+++ src/arrayofobject/Test.java
@@ -4,5 +4,11 @@ public class Test {
 	public static void main(String[] args) {
 		// store and print data of five students.
 		
+		// 1. create array:
+		
+		// 2. write data in array:
+		
+		// 3. read vallues from array:
+		
 	}
 }
