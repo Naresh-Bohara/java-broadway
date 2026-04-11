718abdd37b45fc528dc7a55f0f4bdb4c17f9abb5
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-11 08:16:55 +0545

    updated

--- src/arrayofobject/ArrayOfObjectDocs.java
+++ src/arrayofobject/ArrayOfObjectDocs.java
@@ -3,5 +3,22 @@ package arrayofobject;
 public class ArrayOfObjectDocs {
 	/*
 	 * -------------- Array of Object ------------------------
-	 */
+	 * # syntax:
+	 * 		class_name array_name[] = new class_name[size];
+	 * 		
+	 * 		eg:
+	 * 		int[] values = new int[500];
+	 * 			values[0] = 200;
+	 * 
+	 * 		Student stds[] = new Student[200];
+	 * 			stds[0] = "Hari";  // can't be done
+	 * 			
+	 * 			Student s1 =  new Student();
+	 *			stds[0] = s1;
+	 * 
+	 * 		Cow cows[] = new Cow[50];
+	 * 
+	 * 		Product prods = new Product[200];
+	 * 
+	 * 	 */
 }
