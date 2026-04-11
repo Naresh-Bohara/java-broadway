a1d785cd2a759060805b85340587d43cd13ff57a
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-11 09:55:51 +0545

    updated

--- src/arrayofobject/Student.java
+++ src/arrayofobject/Student.java
@@ -1,5 +1,47 @@
 package arrayofobject;
 
 public class Student {
-
+	private int id;
+	private String fnanme;
+	private String lname;
+	private String college;
+	private int age;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFnanme() {
+		return fnanme;
+	}
+	public void setFnanme(String fnanme) {
+		this.fnanme = fnanme;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getCollege() {
+		return college;
+	}
+	public void setCollege(String college) {
+		this.college = college;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", fnanme=" + fnanme + ", lname=" + lname + ", college=" + college + ", age=" + age
+				+ "]";
+	}
+	
+	
 }
--- src/arrayofobject/Test.java
+++ src/arrayofobject/Test.java
@@ -1,14 +1,58 @@
 package arrayofobject;
 
+import java.util.Scanner;
+
 public class Test {
 	public static void main(String[] args) {
+		
 		// store and print data of five students.
 		
 		// 1. create array:
+		Student stds[] = new Student[5];
+//		
+//		// 2. write data in array:
+//		Student s = new Student();
+//		s.setId(112);
+//		s.setFnanme("Naresh");
+//		s.setLname("Bohara");
+//		s.setCollege("KMC");
+//		s.setAge(23);
+//		
+//		stds[0] = s;
 		
-		// 2. write data in array:
+		Scanner sc = new Scanner(System.in);
+		
+		for(int i=0; i<stds.length; i++) {
+			Student s = new Student();
+			
+			System.out.println("Enter student Id: ");
+			s.setId(sc.nextInt());
+			
+			System.out.println("Enter student First Name: ");
+			s.setFnanme(sc.next());
+			
+			System.out.println("Enter student Last Name: ");
+			s.setLname(sc.next());
+			
+			s.setCollege("KMC");
+			
+			System.out.println("Enter student Age: ");
+			s.setAge(sc.nextInt());
+			
+			stds[i] = s;
+		}
 		
 		// 3. read vallues from array:
-		
+		int count = 0;
+		for(Student std: stds) {
+			System.out.println(std);
+			
+			// to print no of students below age of 25
+			if(std.getAge() < 25) {
+				count +=1;
+			}
+			
+		}
+		System.out.println("The no.of students below age 25 are: "+count);
 	}
 }
