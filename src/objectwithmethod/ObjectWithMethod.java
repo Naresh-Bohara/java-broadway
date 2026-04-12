b9ab189dbd5de438954afea9a95cafb7edf9c79f
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-12 17:40:10 +0545

    updated upto object with method

--- src/objectwithmethod/ObjectWithMethod.java
+++ src/objectwithmethod/ObjectWithMethod.java
@@ -3,6 +3,29 @@ package objectwithmethod;
 public class ObjectWithMethod {
 	/*
 	 * --------------------- object with method ----------------------------
-	 *	
+	 * 1. Object as parameter:
+	 * 		
+	 * 		Customer: id, fn, ln, age,....20 
+	 * 		
+	 * 		void printCustomer(Customer c){
+	 * 			
+	 * 		}
+	 * 
+	 * 		Customer c = new Customer();
+	 * 		// ........
+	 * 		// ........ here set values....
+	 * 
+	 * 		printCustomer(c);
+	 * 
+	 * 2. Object as return type:
+	 * 			
+	 * 		Customer getCustomerData(){
+	 * 			Customer c = new Customer();
+	 * 			//...............
+	 * 			// ........ here set values....
+	 * 
+	 * 			return c;
+	 * 		}
+	 * 
 	 */
 }
