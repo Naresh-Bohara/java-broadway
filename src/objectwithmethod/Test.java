c03d0db149841c07104c3e8ba72fb6e330d0f609
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-13 08:47:06 +0545

    updated

---
+++ src/objectwithmethod/Test.java
@@ -0,0 +1,35 @@
+package objectwithmethod;
+
+public class Test {
+	public static void main(String[] args) {
+		
+		Test t = new Test();
+		Product p = t.getProductData();
+		t.printProduct(p);
+	}
+	
+	// object as parameters:
+	void printProduct(Product p) {
+		System.out.println("Id = "+p.getId());
+		System.out.println("Name = "+p.getName());
+		System.out.println("Price = "+p.getPrice());
+		System.out.println("Company = "+p.getPrice());
+	}
+	
+	// object as return type:
+	Product getProductData() {
+		Product p = new Product();
+		p.setId(112);
+		p.setName("Redmi note-14");
+		p.setPrice(120000);
+		p.setCompany("Redmi");
+		
+		return p;
+	}
+	
+	// array of object as a parameter: 
+	
+	
+	// array of object as return type:
+	
+}
