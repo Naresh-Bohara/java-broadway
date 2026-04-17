53ebe2f7e2cb9ef56b5b60a135c693f71be44fa9
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-17 22:14:02 +0545

    updated

--- src/objectwithmethod/Test.java
+++ src/objectwithmethod/Test.java
@@ -1,11 +1,16 @@
 package objectwithmethod;
 
+//import java.util.Arrays;
+import java.util.Scanner;
+
 public class Test {
 	public static void main(String[] args) {
 		
 		Test t = new Test();
 		Product p = t.getProductData();
 		t.printProduct(p);
+//		System.out.println(Arrays.toString(t.getAllProducts()));
+		t.printAllProducts(t.getAllProducts());
 	}
 	
 	// object as parameters:
@@ -13,7 +18,7 @@ public class Test {
 		System.out.println("Id = "+p.getId());
 		System.out.println("Name = "+p.getName());
 		System.out.println("Price = "+p.getPrice());
-		System.out.println("Company = "+p.getPrice());
+		System.out.println("Company = "+p.getCompany());
 	}
 	
 	// object as return type:
@@ -29,14 +34,31 @@ public class Test {
 	
 	// array of object as a parameter: 
 	void printAllProducts(Product[] prods) {
-		
+		for(Product x:prods) {
+			System.out.println(x);
+		}
 	}
 	
-	// array of object as return type:
+	// array of object as return type: 
 	Product[] getAllProducts() {
-		Product[] products = new Product[4];
 		
-		return null;
+		Product[] products = new Product[3];
+		Scanner sc = new Scanner(System.in);
+		for(int i=0; i<products.length; i++) {
+			Product p = new Product();
+			System.out.println("Please enter product Id: ");
+			p.setId(sc.nextInt());
+			System.out.println("Please enter product Name: ");
+			p.setName(sc.next());
+			System.out.println("Please enter product Price: ");
+			p.setPrice(sc.nextInt());
+			System.out.println("Please enter product Company: ");
+			p.setCompany(sc.next());
+			
+			products[i] = p; 
+		}
+		sc.close();
+		return products;
 		
 	}
 }
