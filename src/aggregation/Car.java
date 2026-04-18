1a0441519fc27e049981035b1e17cf8bc377c8b1
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-18 14:41:39 +0545

    updated

--- src/aggregation/Car.java
+++ src/aggregation/Car.java
@@ -1,5 +1,39 @@
 package aggregation;
 
 public class Car {
-
+	private String name;
+	private String model;
+	private int price;
+	private String color;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getModel() {
+		return model;
+	}
+	public void setModel(String model) {
+		this.model = model;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	@Override
+	public String toString() {
+		return "Car [name=" + name + ", model=" + model + ", price=" + price + ", color=" + color + "]";
+	}
+	
 }
