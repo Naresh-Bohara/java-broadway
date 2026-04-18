2c2539970b371217114ff18b78595b5596c60879
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-18 18:22:05 +0545

    updated

--- src/abstraction/Abstraction.java
+++ src/abstraction/Abstraction.java
@@ -3,6 +3,21 @@ package abstraction;
 public class Abstraction {
 	/*
 	 * -------------------- Abstration ---------------------
-	 * # 
+	 * # process of hiding implementations details in the program.
+	 * # To achieve abstraction:
+	 * 
+	 * 		1. Abstract class:
+	 * 			-> class which contains abstract (unimplemented) methods and non-abstract methods.
+	 * 				# Abstract method:
+	 * 					-> method which doesn't have body/implementation
+	 * 					-> syntax:
+	 * 							abstract retrun_type method_name(arg1, arg2, ...,argn)
+	 * 					-> e.g:
+	 * 							abstract void print();
+	 * 
+	 * 		2. Interface
+	 * 
+	 * 
+	 * 
 	 */
 }
