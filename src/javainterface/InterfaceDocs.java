3c41114184a2326a57e527161b6d2ad72a657985
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-19 13:53:58 +0545

    updated to interface

--- src/javainterface/InterfaceDocs.java
+++ src/javainterface/InterfaceDocs.java
@@ -3,6 +3,20 @@ package javainterface;
 public class InterfaceDocs {
 	/*
 	 * ----------------- Interface ----------------------
+	 * # To achieve abstraction.
+	 * # same as class which contains which contains public abstract methods and public static final 
+	 * 	 variables (i.e constants).
+	 * # every interface must have at least one implementation class and impl. class must ovrride abstract methods.
+	 * # we can't create object.
+	 * # 100% abstraction
+	 * # multiple inheritance support in java with innterface.
+	 * # syntax:
+	 * 		interface interface_name {
 	 * 
+	 * 			// public abstract methods
+	 * 			
+	 * 			// public static final variables
+	 * 
+	 * 		}
 	 */
 }
