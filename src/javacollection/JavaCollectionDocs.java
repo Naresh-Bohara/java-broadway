e01fda8ece8d33650784eb92f24386c1e72b2bdc
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-20 17:33:01 +0545

    updated to java collection

--- src/javacollection/JavaCollectionDocs.java
+++ src/javacollection/JavaCollectionDocs.java
@@ -3,6 +3,20 @@ package javacollection;
 public class JavaCollectionDocs {
 	/*
 	 * ------------ Java Collection / Collection framework ------------------
+	 * # used to store and processing dynamic multiple data/objects.
 	 * 
+	 * 		1. List: dynamic array
+	 * 				i. ArrayList: slow process
+	 * 				ii. LinkedList: fast processing 
+	 * 
+	 * 		2. Set: collection of unique data
+	 * 				i. HashSet: unique data only
+	 * 				ii. TreeSet: unique data + sorted
+	 * 				iii.  LinkedHashSet: unique + insertion in order
+	 * 
+	 * 		3. Map: data manage in key, value pair.
+	 * 				i. HashMap: <k, v> pair
+	 * 				ii. TreeMap: <k, v> pair  + sorted
+	 * 				iii. LinkedHashMap: <k, v> pair + insertion in order
 	 */
 }
