c53748b4b13c602ec485892f0d04776e00e74fbc
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-25 14:02:46 +0545

    updated to database

--- src/database/TestDB.java
+++ src/database/TestDB.java
@@ -1,5 +1,10 @@
 package database;
 
 public class TestDB {
-
+	public static void main(String[] args) throws ClassNotFoundException {
+		// register driver:
+		Class.forName("");
+		
+		// getConnection(url, un, psw)
+	}
 }
