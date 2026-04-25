6cf6662321a97fa4913298017b18e7241bf85827
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-25 14:22:02 +0545

    updated to database

--- src/database/TestDB.java
+++ src/database/TestDB.java
@@ -1,10 +1,26 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class TestDB {
-	public static void main(String[] args) throws ClassNotFoundException {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException   {
 		// register driver:
-		Class.forName("");
+		Class.forName("com.mysql.cj.jdbc.Driver");
 		
 		// getConnection(url, un, psw)
+		Connection con = DriverManager.getConnection(
+			    "jdbc:mysql://localhost:3306/javabroadwaytestdb",
+			    "root",
+			    "Naresh@123#"
+			);
+		
+		// 1.	Insert sql:
+		//		a.	Write sql
+		//		b.	Create statement
+		//		c.	Execute sql
+		//		d.	close
+
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,8 +1,3 @@
-/**
- * 
- */
-/**
- * 
- */
 module corejavabroadway {
+    requires java.sql;
 }
