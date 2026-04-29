67c5dfe2abf715f5a1bf09a682a1a98244b63457
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-29 19:42:24 +0545

    updated

---
+++ src/javaswing/SwingDocs.java
@@ -0,0 +1,20 @@
+package javaswing;
+
+public class SwingDocs {
+	/*
+	 * --------------- Swing Framework ------------------------
+	 * # use to create ui/gui/front end for desktop application in java.
+	 * 
+	 * # UI Component:
+	 * 		- JFrame
+	 * 		- JPanel
+	 * 		- JButton
+	 * 		- JTextField
+	 * 		- JLabel
+	 * 		- JRadioButton
+	 * 		- JCheckBox
+	 * 		- JTable
+	 * 		- JMenu
+	 * 		JPasswordField
+	 */
+}
