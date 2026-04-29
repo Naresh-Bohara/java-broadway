4a35ef041e5b42b85cb3fd7d06cfb499db19ba2a
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-04-29 22:43:28 +0545

    updated

--- src/javaswing/FormTest.java
+++ src/javaswing/FormTest.java
@@ -1,10 +1,21 @@
 package javaswing;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class FormTest {
 	public static void main(String[] args) {
 		JFrame form = new JFrame();
+		
+		form.setSize(600, 400);
+		form.setTitle("Test From");
+		form.setLayout(null);
+		
+		// JLabel:
+		JLabel jl = new JLabel("Good Morining");
+//		jl.setSize(120, 20);
+		jl.setBounds(200, 50, 120, 20);
+		form.add(jl);
 		
 		form.setVisible(true);
 	}
