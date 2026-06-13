8b68a9dcba24e44e20b8d9ba0e57feb047f1a4a4
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-06-13 18:03:09 +0545

    updated upto exception handling

---
+++ src/exc/FinallyTest.java
@@ -0,0 +1,29 @@
+package exc;
+
+import java.util.Scanner;
+
+public class FinallyTest {
+	 public static void main(String[] args) {
+	        FinallyTest obj = new FinallyTest();
+	        obj.finallyCallTest();
+	    }
+
+	    public void finallyCallTest() {
+	        Scanner sc = null;
+	        try {
+	            sc = new Scanner(System.in);
+
+	            System.out.println("Input a int value.");
+	            int val = sc.nextInt(); // Error if you input String value;
+	            System.out.println(val);
+
+	        } catch (ClassCastException e) {
+	            e.printStackTrace();
+	        } finally { // Always executed.
+	            if (sc != null) {
+	                sc.close();
+	            }
+	            System.out.println("Scanner resource Closed.");
+	        }
+	    }
+}
