62bc44702c6d02fbe4ba0537c2cd9633e5290686
Naresh Bohara <116049712+Naresh-Bohara@users.noreply.github.com>
2026-06-16 07:49:02 +0545

    multithreaddding intro

--- src/multithreading/MultithreadingDocs.java
+++ src/multithreading/MultithreadingDocs.java
@@ -1,7 +1,13 @@
 package multithreading;
 
 public class MultithreadingDocs {
-public static void main(String[] args) {
-	
-}
+/*
+ * ------------------- Multi Threading ---------------------
+ * # small part of process which has separate path of execution is known as thread. 
+ * 
+ * # How to create thread 
+ *  a. Thread class
+ *  b. Runnable interface
+ * 	
+ */
 }
